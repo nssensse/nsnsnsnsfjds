fccd555de3eb1f580b33817e871ca01dcd2824ba
Vladimir <nightsense@mail.ru>
2023-10-01 09:42:19 +0300

    dddddsdas

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="z-40 flex justify-between bheader">
-      <Link className="text-white font-bold" href={"/"}>
+      <Link className="font-bold" href={"/"}>
       ✱BONUS BUY✱
       </Link>
     </nav>
