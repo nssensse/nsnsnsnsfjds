f466c41aea45da889798e8f7ad46be4042e94bd8
Vladimir <nightsense@mail.ru>
2023-10-01 09:33:16 +0300

    fdfdddd

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,58 +1,46 @@
-"use client";
+import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
 
-export default function EditTopicForm({ id, title, description }) {
-  const [newTitle, setNewTitle] = useState(title);
-  const [newDescription, setNewDescription] = useState(description);
-
-  const router = useRouter();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ newTitle, newDescription }),
-      });
-
-      if (!res.ok) {
-        throw new Error("Failed to update topic");
-      }
-
-      router.refresh();
-      router.push("/");
-    } catch (error) {
-      console.log(error);
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
     }
-  };
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      <input
-        onChange={(e) => setNewTitle(e.target.value)}
-        value={newTitle}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Title"
-      />
+    <>
+      {topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
 
-      <input
-        onChange={(e) => setNewDescription(e.target.value)}
-        value={newDescription}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Description"
-      />
-
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
-        Update Topic
-      </button>
-    </form>
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
