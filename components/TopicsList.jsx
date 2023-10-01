79488ab0e88ff1d45d8fdc8a5a28dfabdb3dbbde
Vladimir <nightsense@mail.ru>
2023-10-01 12:14:52 +0300

    fdssssss

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -102,8 +102,8 @@ function calc(){
           >
             
             <div className="textout">{t.title}</div>
-            <div className="textout">{t.description + "💸"}</div>
-            <div className="textout">{t.winning + "💸"}</div>
+            <div className="textout">{t.description + ""}</div>
+            <div className="textout">{t.winning + ""}</div>
           </div>
           <div className="pl-80 grid grid-cols-2 gap-60 text-[#edeef0] text-xl font-bold box2">
             <div className="">{t.title}</div>
