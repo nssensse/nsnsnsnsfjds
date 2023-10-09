9f57b1d5459882eb5c59e92ce203d03a1bf08e94
Vladimir <nightsense@mail.ru>
2023-10-09 20:30:09 +0300

    sssss

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,11 +4,11 @@ export default function Navbar() {
   return (
     
     <nav className="z-40 flex justify-between bheader">
-      
+
       <Link className="font-bold" href={"/"}>
       ✟BONUS BUY✟ 
       </Link>
-      <div className="winner">🏆3dinr - 1123x
+      <div className="winner">🏆fov - 809x
       </div>
     </nav>
     
