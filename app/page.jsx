32205d4007f03099b1d760587bca5c90e6b058c7
Vladimir <nightsense@mail.ru>
2023-11-14 17:38:48 +0300

    fsdfsdfdfs

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import TopicsList from "@/components/TopicsList";
 export default function Home() {
-  return<div>Hello!</div>
-  //return <TopicsList />;
+  //return<div>Hello!</div>
+  return <TopicsList />;
 }
